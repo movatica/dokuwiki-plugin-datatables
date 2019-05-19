55db7e46380d8bd61aef3b6fbde657336ffacb0b
movatica <c0d3@movatica.com>
2019-05-19 23:08:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,14 +28,14 @@ jQuery(function() {
 			}
 			
 			while(headerRows > 0) {
-      	headerRows--;
-      	$thead.append($tbody.children().first());
+				headerRows--;
+				$thead.append($tbody.children().first());
 			}
 
 			if (missingThead) {
 				$target_table.prepend($thead);
 			}
-  	}
+		}
 
 		if (jQuery('thead > tr', $target_table).size() && ! jQuery('tbody', $target_table).find('[rowspan], [colspan]').length) {
 			$target_table.attr('width', '100%');
@@ -44,22 +44,21 @@ jQuery(function() {
 	}
 
 	if ('plugin' in JSINFO && 'datatables' in JSINFO.plugin) {
-  	if (JSINFO.plugin.datatables.enableForAllTables) {
-    	jQuery(ALL_TABLES_SELECTOR).each(function() {
-      	var $target_table = jQuery(this).parent();
+		if (JSINFO.plugin.datatables.enableForAllTables) {
+			jQuery(ALL_TABLES_SELECTOR).each(function() {
+				var $target_table = jQuery(this).parent();
 
-      	init_datatables($target_table, JSINFO.plugin.datatables.config);
-    	});
+				init_datatables($target_table, JSINFO.plugin.datatables.config);
+			});
 		} else {
-    	jQuery(WRAP_TABLES_SELECTOR).each(function() {
-      	var $target_table = jQuery(this),
-            dt_config     = jQuery.extend({},
-																					JSINFO.plugin.datatables.config,
-																					jQuery(this).parents('.dt-wrapper').data()
-																				 );
-
-      	init_datatables($target_table, dt_config);
-    	});
+			jQuery(WRAP_TABLES_SELECTOR).each(function() {
+				var $target_table = jQuery(this),
+				    dt_config     = jQuery.extend({},
+				                                  JSINFO.plugin.datatables.config,
+				                                  jQuery(this).parents('.dt-wrapper').data()
+				                                  );
+				init_datatables($target_table, dt_config);
+			});
 		}
 	}
 });
